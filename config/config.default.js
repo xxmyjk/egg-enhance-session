7650567d962430e71f858039da9a99b4fc1e09ff
Jin Yi <xxmyj.k@gmail.com>
2018-03-29 09:46:13 +0800

    chore: init project

---
+++ config/config.default.js
@@ -0,0 +1,10 @@
+'use strict';
+
+/**
+ * egg-enhance-session default config
+ * @member Config#enhanceSession
+ * @property {String} SOME_KEY - some description
+ */
+exports.enhanceSession = {
+
+};
